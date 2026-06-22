f404e1567dda0ec686fad9161dbccee918bfc01d
Adam Babangida <adambabangida14.com>
2026-06-22 10:12:03 +0100

    update .toml file

--- api/database.py
+++ api/database.py
@@ -5,8 +5,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
 load_dotenv()
 
-# postgresql://postgres:devhatch1@netmedika.c2fqkemcsrnr.us-east-1.rds.amazonaws.com:5432/netmedika
-
 engine = create_engine(os.getenv("DATABASE_URL", "sqlite:///./test.db"))
 SessionLocal = sessionmaker(
     autocommit=False, 
